370a7cab91506b080df06df022a7c7ae212344d5
maraguastini <73542485+maraguastini@users.noreply.github.com>
2021-01-24 16:21:03 +0100

    Update mioprogetto.cpp

--- newfolder/mioprogetto.cpp
+++ newfolder/mioprogetto.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Ciao Gemma!";
+    std::cout << "Ciao Gemma ehiiiiii!";
     std::cout << "sono sulla nuova branch!";
     std::cout << "se io cambio una cosa qui";
-    std::cout << "sono sulla nuova branch!!!";
+    std::cout << "sono sulla nuova nuooova branch!!!";
     std::cout << "eh nsì!";
     std::cout << "ma la cambio anche qui, che succede?";
     return 0;
